7ce0cd73ec46390709f64032ea4e6de652d1839a
mahmoud2121996 <mahmoudasim74@gmail.com>
2020-04-13 01:52:41 +0200

    uploading delete order

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,3 +1,6 @@
 class Order < ApplicationRecord
     Meal = ['break fast', 'launch', 'launch2']
+    belongs_to :user 
+    has_many :invited_users
+    has_many :users, through: :invited_users
 end
