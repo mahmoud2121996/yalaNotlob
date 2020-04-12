b18f7b8dc9c47978164a77edd92db225898bc3af
Mohamed-Zkaria <mohamed.zakaria_10_7@yahoo.com>
2020-04-13 00:32:22 +0200

    Displaying error and success messages when user adds a friend.

--- app/models/friend.rb
+++ app/models/friend.rb
@@ -1,4 +1,7 @@
 class Friend < ApplicationRecord
-    validates :user_id, uniqueness: { scope: [:user_id, :friend_id] }
+    validates :user_id, presence: true
+    validates :friend_id, presence: true
+    
+    validates :friend_id, uniqueness: { scope: [:user_id, :friend_id] }
     belongs_to :user
 end
