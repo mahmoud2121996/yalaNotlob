feea79b1191bf9475db7c026329c4e09874b0758
MABasha94 <Mahmoud-Elbasha@outlook.com>
2020-04-13 10:24:54 +0200

    fix some issue in login and registration

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   default_url_options :host => "localhost:3000"
   devise_for :users, controllers: { omniauth_callbacks: 'omniauth_callbacks' }
+  
   root to: 'user#index'
 
   get '/friends', to: 'friend#friends_index'
