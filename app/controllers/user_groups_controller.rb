8ce6c8a601b07fa7fc26f7e0e5b8538b1ad4c276
Eman <emanmagdy961@gmail.com>
2020-04-14 17:29:56 +0200

    test groups

--- app/controllers/user_groups_controller.rb
+++ app/controllers/user_groups_controller.rb
@@ -1,4 +1,8 @@
 class UserGroupsController < ApplicationController
+    def index
+        render "groups/index"
+    end
+
     def create
         @user = User.find_by(email: params[:email])
             if @user.nil?
@@ -8,20 +12,32 @@ class UserGroupsController < ApplicationController
             else
                 @user_group = UserGroup.find_by(user_id: @user.id, group_id: params[:group_id])
                 if @user_group.nil?
-                    @user_group = UserGroup.create(user_id: @user.id, group_id: params[:group_id])
-                    unless @user_group.persisted?
+                    @friend = Friend.find_by(user_id: current_user.id, friend_id: @user.id)
+                    unless @friend.nil?
+                         @user_group = UserGroup.create(user_id: @user.id, group_id: params[:group_id])
+                unless @user_group.persisted?
                         flash[:user_group_errors] = @user_groups.errors.full_messages
                     end
+                else
+                    flash[:group_member_errors] = ["User isn't a friend"]
+                end 
+
                 else
                     flash[:user_group_errors] = "User Already Exist in this Group"
                 end
             end
             redirect_to group_url(:id => params[:group_id])
-      end
+    end
     
-      def destroy
-        @users = UserGroup.where(group_id: params[:id], user_id: params[:user_id])
-            @users.destroy_all
+    def destroy
+        @group = Group.find_by(id: params[:id], user_id: current_user.id)
+        unless @group.nil?
+            @members = UserGroup.where(group_id: params[:id], user_id: params[:user_id])
+            @members.destroy_all
             redirect_to group_url(params[:id])
-      end
+        else
+            flash[:group_error] = "You can't delete this group"
+            redirect_to :groups
+        end    
+    end
 end
