f44e171d04beba8e673b2d9637ef2a77f63360c0
mahmoud2121996 <mahmoudasim74@gmail.com>
2020-04-14 08:26:45 +0200

    uploading order details

--- app/controllers/order_details_controller.rb
+++ app/controllers/order_details_controller.rb
@@ -1,8 +1,23 @@
 class OrderDetailsController < ApplicationController
   def index
+    @user = current_user
+    @details = OrderDetail.where(order_id: params[:order])
+    @order = Order.find(params[:order])
+    @newDetail = OrderDetail.new
+    @users = OrderDetail.where(order_id: params[:order])
+    @invited_users = InvitedUser.where(order_id: params[:order])
   end
 
   def create
+  @detail = OrderDetail.new 
+  @detail.user_id = current_user.id
+  @detail.order_id = params[:order_id]
+  @detail.item_name = params[:item]
+  @detail.amount = params[:amount]
+  @detail.price = params[:price]
+  @detail.comment = params[:comment]
+  @detail.save
+  redirect_to :controller => 'order_details', :action => 'index' ,:order => params[:order_id]
   end
 
   def new
@@ -18,5 +33,9 @@ class OrderDetailsController < ApplicationController
   end
 
   def destroy
+    @detail = OrderDetail.find(params[:id])
+    id = @detail.order_id
+    @detail.delete
+    redirect_to :controller => 'order_details', :action => 'index' ,:order => id
   end
 end
